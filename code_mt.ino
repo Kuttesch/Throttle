ef04c87aedcc7cb7b996e04a1a06fc8cf0c53d36
Kuttesch <106610385+Kuttesch@users.noreply.github.com>
2024-05-09 19:49:05 +0200

    updated core1 in code_mt.ino

--- code_mt.ino
+++ code_mt.ino
@@ -38,7 +38,7 @@ int buttons[] = {20, 21, 22, 0, 1, 2, 5, 4, 3};                                 
     void loop1() {
         //Joystick
             Joystick.Z(constrain(map(analogRead(joystick1), 110, 990, 0, 1000), 0, 1000));         // Usage: Joystick.'axis'(constrain(map(analogRead('pin'), 'min', 'max', 'min', 'max'), 'min', 'max'));
-            Joystick.sliderRight(constrain(map(analogRead(joystick2), 600, 860, 0, 1000), 0, 1000));         // Usage: Joystick.'axis'(constrain(map(analogRead('pin'), 'min', 'max', 'min', 'max'), 'min', 'max'));
+            Joystick.sliderRight(constrain(map(analogRead(joystick2), 560, 860, 0, 1000), 0, 1000));         // Usage: Joystick.'axis'(constrain(map(analogRead('pin'), 'min', 'max', 'min', 'max'), 'min', 'max'));
             Joystick.sliderLeft(constrain(map(analogRead(joystick2), 30, 440, 1000, 0), 0, 1000));          // Usage: Joystick.'axis'(constrain(map(analogRead('pin'), 'min', 'max', 'min', 'max'), 'min', 'max'));
 
             Joystick.send_now();                                                                   // Send the joystick state to the host computer
@@ -46,7 +46,7 @@ int buttons[] = {20, 21, 22, 0, 1, 2, 5, 4, 3};                                 
 
         //Dev
             int zValue = constrain(map(analogRead(joystick1), 110, 990, 0, 1000), 0, 1000);        // Read the Z-axis value from the joystick
-            int yValue = constrain(map(analogRead(joystick2), 600, 860, 0, 1000), 0, 1000);        // Read the Rz-axis value from the joystick
+            int yValue = constrain(map(analogRead(joystick2), 560, 860, 0, 1000), 0, 1000);        // Read the Rz-axis value from the joystick
             int xValue = constrain(map(analogRead(joystick2), 30, 440, 1000, 0), 0, 1000);  
             //Serial.print("z: ");
             //Serial.print(zValue);
@@ -56,5 +56,4 @@ int buttons[] = {20, 21, 22, 0, 1, 2, 5, 4, 3};                                 
             Serial.println(xValue);
         //Dev
     }
-
 //Core1
